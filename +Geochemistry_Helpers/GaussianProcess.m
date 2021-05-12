9294ae6c1d8bb0833be6ad9433a8792cf23121f4
rossidae <rossdmw@gmail.com>
2021-05-12 09:39:04 +0100

    Adding methods for reweighting.

--- +Geochemistry_Helpers/GaussianProcess.m
+++ +Geochemistry_Helpers/GaussianProcess.m
@@ -1,8 +1,9 @@
-classdef GaussianProcess < handle
+classdef GaussianProcess < handle & Geochemistry_Helpers.Collator
     properties
         kernel
         parameters
         observations
+        observation_approximations
         queries = Geochemistry_Helpers.Distribution();
         covariance_matrix
         observation_covariance_matrix
@@ -35,16 +36,18 @@ classdef GaussianProcess < handle
             self.covariance_matrix = self.kernel_function(self.queries.collate("location"),self.queries.collate("location")',self.parameters);
             if ~isempty(self.observations) && ~isempty(self.observations.collate("location"))
                 self.conditioned = true;
+                self.observation_approximations = self.observations.approximateGaussian(2);
+            
                 
                 self.observation_covariance_matrix = self.kernel_function(self.observations.collate("location"),self.observations.collate("location")',self.parameters)';
                 self.cross_covariance_matrix = self.kernel_function(self.observations.collate("location")',self.queries.collate("location"),self.parameters)';
                 self.query_covariance_matrix = self.kernel_function(self.queries.collate("location"),self.queries.collate("location")',self.parameters)';
             
                 self.observation_covariance_matrix = self.observation_covariance_matrix+diag(repelem(1e-12,size(self.observation_covariance_matrix,1)));
-                sigma_eye_1 = (self.observations.standard_deviation()*5).*eye(size(self.observation_covariance_matrix));
+                observation_uncertainty = (self.observation_approximations.standard_deviation().^2).*eye(size(self.observation_covariance_matrix));
                 
-                self.means = mean(self.observations.mean()) + self.cross_covariance_matrix/(self.observation_covariance_matrix+sigma_eye_1)*(self.observations.mean()-mean(self.observations.mean()));
-                self.covariances = self.query_covariance_matrix-self.cross_covariance_matrix/(self.observation_covariance_matrix+sigma_eye_1)*self.cross_covariance_matrix';
+                self.means = mean(self.observation_approximations.mean()) + self.cross_covariance_matrix/(self.observation_covariance_matrix+observation_uncertainty)*(self.observation_approximations.mean()-mean(self.observation_approximations.mean()));
+                self.covariances = self.query_covariance_matrix-self.cross_covariance_matrix/(self.observation_covariance_matrix+observation_uncertainty)*self.cross_covariance_matrix';
             end
         end
         function self = getSamples(self,number_of_samples)
@@ -55,40 +58,83 @@ classdef GaussianProcess < handle
             end
             self.assignSamples();
         end
-        function reweight(self,limit)
-            if nargin==1
-                limit = 1e6;
-            end
+        function reweight(self,method)
             samples_at_observations = interp1(self.queries.collate("location"),self.samples',self.observations.collate("location")');
-            samples_at_observations_histograms = NaN(size(samples_at_observations,1),numel(self.observations(1).bin_edges)-1);
-            for observation_index = 1:size(samples_at_observations,1)
-                [samples_at_observations_histograms(observation_index,:),~,samples_at_observations_histograms_indices(observation_index,:)] = histcounts(samples_at_observations(observation_index,:),self.observations(1).bin_edges,'Normalization','Probability');
+            
+            if strcmp(method,"rejection")
+                supremum = self.getSupremum();
+                new_samples = [];
+                % What's the chance of getting those samples
+                while size(new_samples,1)<size(self.samples,1)
+                    for sample_index = 1:size(self.samples,1)
+                        observation_probability = self.observations.getProbability(samples_at_observations(:,sample_index));
+                        observation_approximation_probability = self.observation_approximations.getProbability(samples_at_observations(:,sample_index));
+                        
+                        accept = prod(observation_probability)./(prod(observation_approximation_probability)*supremum);
+                        if rand(1)<accept
+                            new_samples = [new_samples;self.samples(sample_index,:)];
+                        end
+                    end
+                    disp(size(new_samples,1)+" out of "+size(self.samples,1));
+                end
+                
+                
+            elseif strcmp(method,"weight")
+                observation_probability = NaN(size(samples_at_observations));
+                observation_approximation_probability = NaN(size(samples_at_observations));
+                for sample_index = 1:size(self.samples,1)
+                    observation_probability(:,sample_index) = self.observations.getProbability(samples_at_observations(:,sample_index));
+                    observation_approximation_probability(:,sample_index) = self.observation_approximations.getProbability(samples_at_observations(:,sample_index));
+                end
+                observations_probability_cumulative = prod(observation_probability,1);
+                observation_approximations_probability_cumulative = prod(observation_approximation_probability,1);
+                
+                ratio = observations_probability_cumulative./observation_approximations_probability_cumulative;
+                ratio_normalised = ratio./sum(ratio);
+                [ratio_normalised_sorted,ratio_index] = sort(ratio_normalised);
+                
+                ratio_sampler = Geochemistry_Helpers.Sampler([ratio_index-0.5,ratio_index(end)+0.5],"manual",ratio_normalised_sorted,"latin_hypercube");
+                ratio_sampler.getSamples(10000);
+                rounded_samples = round(ratio_sampler.samples);
+                new_samples = self.samples(rounded_samples,:);
+            else
+                error("Method unknown");
             end
-            actual_observations_histograms = self.observations.collate("probabilities");
             
-            weights = actual_observations_histograms./samples_at_observations_histograms;
-            weights(weights>limit)=NaN;
-            weights(isnan(weights)) = 0;
-            
-            self.observation_weights = weights;
-            self.query_weights =  interp1(self.observations.collate("location"),self.observation_weights,self.queries.collate("location"),'linear','extrap');
-            
+            original_queries = self.queries.copy();
+            self.samples = new_samples;
             self.assignSamples();
             
-%             clf
-%             subplot(2,1,1);
-%             hold on            
-%             plot(self.observations(1),'r');
-%             plot(self.observations(1).bin_midpoints,samples_at_observations_histograms(1,:),'g');
-%             plot(self.queries(6).bin_midpoints,self.queries(6).probabilities,'b');
-%             
-%             subplot(2,1,2);
-%             hold on
-%             plot(self.observations(8),'r');
-%             plot(self.observations(8).bin_midpoints,samples_at_observations_histograms(8,:),'g');            
-%             plot(self.queries(8).bin_midpoints,self.queries(366).probabilities,'b');
+            %%
+            index = 1;
+            figure(1);
+            clf
+            hold on
+            self.observations(index).plot('r');
+            self.observation_approximations(index).plot('g');
+            original_queries(round(original_queries(index).location*10 + 1)).plot('b');
+            self.queries(round(self.observations(index).location*10 + 1)).plot('k');
+
+            %%
+            figure(2);
+            clf
+            hold on
+            plot(self.queries.collate("location"),new_samples(1:100,:));
+            
+            plot(self.observations.collate("location"),self.observations.mean(),'ok','MarkerFaceColor','k');
         end
         
+        function supremum = getSupremum(self)
+            for distribution_index = 1:numel(self.observations)
+                quantiles = self.observations(distribution_index).quantile([0.005,0.025,0.05,0.1,0.9,0.95,0.975,0.995]);
+                observation_quantile_likelihoods = self.observations(distribution_index).getProbability(quantiles);
+                observation_approximation_quantile_likelihoods = self.observation_approximations(distribution_index).getProbability(quantiles);
+            
+                ratio(:,distribution_index) = observation_quantile_likelihoods./observation_approximation_quantile_likelihoods;
+            end
+            suprema = max(ratio,[],1);
+            supremum = prod(suprema);
+        end
         function assignSamples(self)
             locations = self.queries.collate("location");
             for query_index = 1:numel(self.queries)
